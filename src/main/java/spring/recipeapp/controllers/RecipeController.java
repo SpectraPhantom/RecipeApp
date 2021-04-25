51c475e1966e39c7c821241ebd09315a9bd4b4ad
SpectraPhantom <kbarszczyk@protonmail.com>
2021-04-26 00:47:13 +0200

    Added PostMaping in RecipeController and Recipe Form.

--- src/main/java/spring/recipeapp/controllers/RecipeController.java
+++ src/main/java/spring/recipeapp/controllers/RecipeController.java
@@ -2,8 +2,8 @@ package spring.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import spring.recipeapp.commands.RecipeCommand;
 import spring.recipeapp.services.RecipeService;
 
 @Controller
@@ -19,5 +19,19 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe",recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand saveCommand=recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + saveCommand.getId();
     }
 }
