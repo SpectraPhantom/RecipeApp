0e729537a440fe3500abd7a02380425758e0486c
SpectraPhantom <kbarszczyk@protonmail.com>
2021-04-26 19:20:17 +0200

    added update recipe function

--- src/test/java/spring/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/spring/recipeapp/controllers/RecipeControllerTest.java
@@ -37,7 +37,7 @@ class RecipeControllerTest {
      MockMvc mockMvc=MockMvcBuilders.standaloneSetup(controller).build();
      when(recipeService.findById(anyLong())).thenReturn(recipe);
 
-     mockMvc.perform(get("/recipe/show/1"))
+     mockMvc.perform(get("/recipe/1/show"))
              .andExpect(status().isOk())
              .andExpect(view().name("recipe/show"));
     }
